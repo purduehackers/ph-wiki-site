56af0ec2604c1101317c2be5b6542c82795ae86d
chi-wei-lien <wilsonwilson3124@gmail.com>
2023-11-01 09:00:00 -0400

    change getAll to getAllMenuItems

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -3,6 +3,6 @@ import { NextRequest, NextResponse } from 'next/server'
 import { postRepo } from '@/db/repo/post'
 
 export const GET = async (req: NextRequest) => {
-  const posts = await postRepo.getAll()
+  const posts = await postRepo.getAllMenuItems()
   return NextResponse.json(posts, { status: 200 })
 }
