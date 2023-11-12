b4d997dd7907b6104e37803082dca372e49ee61e
chi-wei-lien <wilsonwilson3124@gmail.com>
2023-11-12 00:00:03 -0500

    add triangle

---
+++ app/components/wiki/navbar/MenuList.tsx
@@ -0,0 +1,75 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+
+import menuItemI from '@/interfaces/MenuItem'
+import MenuItem from '@/interfaces/MenuItem'
+
+import styles from './styles.module.css'
+
+interface MenuListProps {
+  menuItems: MenuItem[]
+}
+
+const MenuList = ({ menuItems }: MenuListProps) => {
+  return (
+    <div>
+      {menuItems.map((menuItem: menuItemI) => {
+        if (menuItem.type === 'path') {
+          return (
+            <div key={menuItem.id}>
+              <MenuListPath menuItem={menuItem} />
+            </div>
+          )
+        } else {
+          return (
+            <div key={menuItem.id} className={styles.menuItemFile}>
+              <Link href={'/wiki/' + menuItem.slug}>{menuItem.name}</Link>
+            </div>
+          )
+        }
+      })}
+    </div>
+  )
+}
+
+interface MenuListPathProps {
+  menuItem: MenuItem
+}
+
+const MenuListPath = ({ menuItem }: MenuListPathProps) => {
+  const [open, setOpen] = useState<boolean>(false)
+
+  const triangleStyle = {
+    transform: open ? 'rotate(90deg)' : '',
+    transition: 'transform 150ms ease',
+  }
+
+  return (
+    <div
+      key={menuItem.id}
+      className={styles.menuItemFolder}
+      onClick={() => {
+        setOpen(!open)
+      }}
+    >
+      <div className={styles.menuItemFolderLink}>
+        <div style={triangleStyle}>
+          <Image
+            src="/icons/triangle.png"
+            alt="triangle"
+            width={10}
+            height={10}
+          />
+        </div>
+        <div className={styles.menuItemFolderName}>{menuItem.name}</div>
+      </div>
+      <div className={styles.indent}>
+        {open && <MenuList menuItems={menuItem.children} />}
+      </div>
+    </div>
+  )
+}
+
+export default MenuList
--- app/components/wiki/navbar/Navbar.tsx
+++ app/components/wiki/navbar/Navbar.tsx
@@ -1,35 +1,11 @@
-import Link from 'next/link'
-
 import { PathRepo } from '@/db/repo/PathRepo'
-import menuItemI from '@/interfaces/MenuItem'
-import MenuItem from '@/interfaces/MenuItem'
 
+import MenuList from './MenuList'
 import styles from './styles.module.css'
 
 const getRootMenuItem = async () => {
   const rootMenuItem = PathRepo.getRootMenuItem()
   return rootMenuItem
-}
-
-interface MenuListProps {
-  menuItems: MenuItem[]
-}
-
-const MenuList = ({ menuItems }: MenuListProps) => {
-  return (
-    <ul>
-      {menuItems.map((menuItem: menuItemI) => {
-        return (
-          <li key={menuItem.id}>
-            <Link href={'/wiki/' + menuItem.slug}>{menuItem.name}</Link>
-            {menuItem.children.length > 0 && (
-              <MenuList menuItems={menuItem.children} />
-            )}
-          </li>
-        )
-      })}
-    </ul>
-  )
 }
 
 const Navbar = async () => {
