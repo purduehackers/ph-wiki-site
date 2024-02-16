9868f33d637c66ba8c28c5ee3945c0216f094c54
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-02-16 10:50:20 -0500

    setup dev env

--- db/db.ts
+++ db/db.ts
@@ -6,14 +6,29 @@ import { createPathModel } from './model/Path'
 
 const { serverRuntimeConfig } = getConfig()
 
-mongoose
-  .connect(process.env.MONGODB_URI || serverRuntimeConfig.connectionString)
-  .then(() => {
-    // console.log('Connected to MongoDB')
-  })
-  .catch(() => {
-    console.log("Couldn't connect to MongoDB")
-  })
+if (process.env.NODE_ENV == 'production') {
+  mongoose
+    .connect(process.env.MONGODB_URI || serverRuntimeConfig.connectionString)
+    .then(() => {
+      // console.log('Connected to MongoDB')
+    })
+    .catch(() => {
+      console.log("Couldn't connect to MongoDB")
+    })
+} else {
+  mongoose
+    .connect(
+      `mongodb://127.0.0.1:27017/${process.env.DEV_DB_NAME}` ||
+        serverRuntimeConfig.connectionString
+    )
+    .then(() => {
+      // console.log('Connected to MongoDB')
+    })
+    .catch(() => {
+      console.log("Couldn't connect to MongoDB")
+    })
+}
+
 mongoose.Promise = global.Promise
 
 export const db = {
